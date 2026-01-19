5854320ccce51f37b1a4f14eefd08e4ea26da2d1
syedmqasim029 <syedmqasim0321@gmail.com>
2026-01-19 15:21:17 +0500

    Day2

---
+++ Matplotlib Adv Corey Schafer/Matplotlib_tut_02_(Part 2).py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+data = pd.read_csv('/Users/tabindahasif/Downloads/Data Science/kaggle_NoteBooks/Matplotlib Adv Corey Schafer/data.csv').to_dict(orient='records')
+
+
