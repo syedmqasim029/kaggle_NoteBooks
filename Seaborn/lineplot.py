7979e63017260917fa4e0f6c5f0ff10ce363226f
syedmqasim029 <syedmqasim0321@gmail.com>
2026-01-21 22:21:05 +0500

    Line plot

--- Seaborn/lineplot.py
+++ Seaborn/lineplot.py
@@ -1,2 +1,6 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+data = sns.load_dataset('penguins')
+plt.style.use('fivethirtyeight')
+sns.lineplot(x="bill_length_mm",y='bill_depth_mm',data=data,hue="sex",style='sex',palette='Accent')
+plt.show()
